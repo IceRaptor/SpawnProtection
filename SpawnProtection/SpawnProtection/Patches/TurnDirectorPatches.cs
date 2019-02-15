8d3e47bf7be5d3ac23e0e377c1227fe14caef75b
Gerrit Kitts <gkitts@gmail.com>
2019-02-15 15:53:01 -0500

    Fix to always apply on first turn.

--- SpawnProtection/SpawnProtection/Patches/TurnDirectorPatches.cs
+++ SpawnProtection/SpawnProtection/Patches/TurnDirectorPatches.cs
@@ -4,12 +4,12 @@ using SpawnProtection.Helper;
 
 namespace SpawnProtection.Patches {
 
-    [HarmonyPatch(typeof(TurnDirector), "OnFirstContact")]
-    public class TurnDirectorOnFirstContactPatch {
+    [HarmonyPatch(typeof(TurnDirector), "BeginNewRound")]
+    public class TurnDirector_BeginNewRound {
         static void Postfix(TurnDirector __instance) {
             SpawnProtection.Logger.Log($"Protecting lances on firstContact during first round:{__instance.CurrentRound}");
-            if (__instance.CurrentRound == 0 && __instance.DoAnyUnitsHaveContactWithEnemy) {
-                ProtectionHelper.ProtectOnFirstRound(); ;
+            if (__instance.CurrentRound == 1) {
+                ProtectionHelper.ProtectOnFirstRound();
             }
         }
     }
